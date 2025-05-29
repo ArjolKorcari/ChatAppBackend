acb61f3e9c553c7c31866d7fea3509f556149be6
ArjolKorcari <152211082+ArjolKorcari@users.noreply.github.com>
2025-05-29 14:58:35 +0200

    commit client changes

--- src/main/java/com/chatApp/ChatApp/common/mqtt/MqttChatClient.java
+++ src/main/java/com/chatApp/ChatApp/common/mqtt/MqttChatClient.java
@@ -7,13 +7,13 @@ public class MqttChatClient {
     private static final String CLIENT_ID = MqttClient.generateClientId();
 
     public static void main(String[] args) throws MqttException {
-        MqttClientService client = new MqttClientService();
+        MqttClient client = new MqttClient(BROKER, CLIENT_ID);
+        client.connect();
 
+        client.subscribe("chat/5.unique-conversation-id", (topic, msg) -> {
+            System.out.println("Received: " + new String(msg.getPayload()));
+        });
 
-//        client.subscribe("chat/room1", (topic, msg) -> {
-//            System.out.println("Received: " + new String(msg.getPayload()));
-//        });
-
-        client.publishMessage("chat/room1", "Hello from Java!");
+        //client.publish("chat/room1", new MqttMessage("Hello from Java!".getBytes()));
     }
 }
